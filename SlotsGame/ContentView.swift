a3afaaeb92935b3c85819e958a49d8c7efbe7aee
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-03-05 23:08:20 +0100

    Add credits system and dynamic slot logic
    Introduced a credits integer starting at 600 and three slot variables (slot1, slot2, slot3) to manage slot images
    Implemented ScoreLogic() to randomly change slot images and update credits (+100 on match, –50 otherwise)
    Added devOption() for debugging or forced slot setups
    Updated the UI to display current credits and changed the spin button’s label to “Feeling lucky? Spin!”

--- SlotsGame/ContentView.swift
+++ SlotsGame/ContentView.swift
@@ -9,48 +9,89 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var credits: Int = 600
+    
+    @State private var slot1 = "slot1"
+    @State private var slot2 = "slot2"
+    @State private var slot3 = "slot3"
+        
     var body: some View {
         
         VStack {
+            
             Spacer()
+            
             Text("SwiftUI Slots Game")
                 .font(.largeTitle)
             
             Spacer()
             
-            Text("Score/Credits")
+            Text("Credits are now: \(credits)")
+            
             Spacer()
+            
             HStack {
                 
-                Image("001-banana")
+                Image("\(slot1)")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 
-                Image("001-banana")
+                Image("\(slot2)")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 
-                Image("001-banana")
+                Image("\(slot3)")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 
             }.padding()
             
             Spacer()
-            Button("Spin the slot!") {
-                print("Spinned")
+            
+            Button("Feeling lucky? Spin!") {
+                ScoreLogic()
             }
             .buttonStyle(.bordered)
             .background(Color.yellow)
-            .clipShape(Capsule())
-            .shadow(radius: 5)
+            
             Spacer()
             
         }
         
+    }
+    
+    private func devOption() {
+        slot1 = "slot1"
+        slot2 = "slot1"
+        slot3 = "slot1"
+    }
+    private func ScoreLogic() {
+        let slot1Rand = Int.random(in: 1...9)
+        let slot2Rand = Int.random(in: 1...9)
+        let slot3Rand = Int.random(in: 1...9)
+        
+        slot1 = "slot\(slot1Rand)"
+        slot2 = "slot\(slot2Rand)"
+        slot3 = "slot\(slot3Rand)"
+        
+        //                devOption()
+        
+        if credits == 0 {
+            print( "You ran out of credits!")
+            return
+        }
+        
+        if slot1 == slot2 && slot2 == slot3 {
+            credits += 100
+        } else {
+            credits -= 50
+        }
+        print("Spinned")
     }
 }
 
 #Preview {
     ContentView()
 }
+
+//TODO: Reset game when credits reaches 0?
