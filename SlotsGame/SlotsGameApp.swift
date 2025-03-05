58f345945ee0459f3e80aa787600f1b5594d9b90
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-03-05 19:34:06 +0100

    Initial Commit

---
+++ SlotsGame/SlotsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsGameApp.swift
+//  SlotsGame
+//
+//  Created by Nicholas Samuelsson Jeria on 2025-03-05.
+//
+
+import SwiftUI
+
+@main
+struct SlotsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
